188a2e3093fcf6e2f8e2f93bd89f6e0404b20687
Mouad Debbar <mouad.debbar@gmail.com>
2026-09-08 14:08:27 -0400

    testing app..

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -4,8 +4,45 @@
 
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(
-  const Center(
-    child: Text('Hello, world!', key: Key('title'), textDirection: TextDirection.ltr),
-  ),
-);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20.0,
+        children: [
+          Text(
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, fontStyle: FontStyle.normal),
+          ),
+          Text(
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          ),
+        ],
+      ),
+    );
+  }
+}

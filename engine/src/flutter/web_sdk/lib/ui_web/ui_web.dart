1b82d399c7037b6cd10696d981b7afbc72366a52
Mouad Debbar <mouad.debbar@gmail.com>
2026-09-08 15:25:03 -0400

    Cleanup

---
+++ engine/src/flutter/web_sdk/lib/ui_web/ui_web.dart
@@ -0,0 +1,9 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Forwarder for dart:ui_web to package:ui.
+library dart.ui_web;
+
+// ignore: depend_on_referenced_packages
+export 'package:ui/ui_web/src/ui_web.dart';
